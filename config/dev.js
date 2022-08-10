cb9d7e31281d4d262ed90b1e0b279d4773ffbe6c
nuintun <nuintun@qq.com>
2022-08-10 16:49:58 +0800

    chore: Update tools

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/no-commonjs */
 
 module.exports = {
+  env: {
+    NODE_ENV: JSON.stringify('development')
+  },
   cache: {
     enable: true,
     name: 'development'
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/no-commonjs */
 
 module.exports = {
+  env: {
+    NODE_ENV: JSON.stringify('production')
+  },
   cache: {
     enable: true,
     name: 'production'
