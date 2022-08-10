6e986ab54700e109c5ad110ffe00f2b9da038d7e
nuintun <nuintun@qq.com>
2022-08-10 16:48:44 +0800

    feat: Update code

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,6 @@
 /* eslint-disable import/no-commonjs */
 
 module.exports = {
-  env: {
-    NODE_ENV: JSON.stringify('development'),
-    BASE_URL: JSON.stringify('http://127.0.0.1:4004')
-  },
   cache: {
     enable: true,
     name: 'development'
